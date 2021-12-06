acaad044c8213b4cc4af8eb7b731a0ba69c8a5a7
NikolaPedro <atron4ik@gmail.com>
2021-12-06 21:14:30 +1000

    Нереальный круг делает кругаля под четкий дабстеп

--- main.cpp
+++ main.cpp
@@ -6,8 +6,13 @@ int main()
 {
     Camera camera;
 
-    for (size_t i = 0; i < 10000; i++)
-        camera.draw(i);
+    Transform objects[] = { 
+        Sphere(5, Vector3(10, 2, 0)), 
+        Cube(5, Vector3(10, 2, 3))
+    };
+
+    while (true)
+        camera.draw();
 
     getchar();
 }
